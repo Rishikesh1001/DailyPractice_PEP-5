46b5600115ab4f21a3e09cd71c4ffa2ec2509546
Rishikesh Yadav <152897247+Rishikesh1001@users.noreply.github.com>
2026-06-25 01:15:10 +0530

    Add files via upload

---
+++ Evenorodd.java
@@ -0,0 +1,17 @@
+//package PEP2;
+import java.util.*;
+
+public class Evenorodd {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if(x%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+
+        }
+    }
+    
+}
